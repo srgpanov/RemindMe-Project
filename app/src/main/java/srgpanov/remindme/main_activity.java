717512b34524a1d3b7f59bd133fcee684f98f144
srgpanov <srgpanov@rambler.ru>
2016-06-10 18:15:27 +0400

    Create Toolbar

--- app/src/main/java/srgpanov/remindme/main_activity.java
+++ app/src/main/java/srgpanov/remindme/main_activity.java
@@ -3,6 +3,8 @@ package srgpanov.remindme;
 import android.app.Activity;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 import com.google.android.gms.appindexing.Action;
 import com.google.android.gms.appindexing.AppIndex;
@@ -12,9 +14,25 @@ import com.google.android.gms.common.api.GoogleApiClient;
  * Created by Пан on 31.05.2016.
  */
 public class main_activity extends Activity {
+    private Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initToolbar();
+    }
+
+    private void initToolbar() {
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.app_name);
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener(){
+            @Override
+            public boolean onMenuItemClick (MenuItem menuItem){
+                return false;
+            }
+
+        });
+        toolbar.inflateMenu(R.menu.menu);
+
     }
 }
