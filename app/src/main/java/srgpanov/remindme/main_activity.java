0623a81ebbc81dfd04c993bda8846bd08684a29b
srgpanov <srgpanov@rambler.ru>
2016-06-14 19:54:15 +0400

    Upgrade NavigationView

--- app/src/main/java/srgpanov/remindme/main_activity.java
+++ app/src/main/java/srgpanov/remindme/main_activity.java
@@ -3,6 +3,8 @@ package srgpanov.remindme;
 import android.app.Activity;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 
@@ -13,13 +15,19 @@ import com.google.android.gms.common.api.GoogleApiClient;
 /**
  * Created by Пан on 31.05.2016.
  */
-public class main_activity extends Activity {
+public class main_activity extends AppCompatActivity {
+
+    private  static final int LAYOUT = R.layout.activity_main;
     private Toolbar toolbar;
+    private DrawerLayout drawerLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppDefault);
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(LAYOUT);
         initToolbar();
+        initNavigationView();
     }
 
     private void initToolbar() {
@@ -30,9 +38,13 @@ public class main_activity extends Activity {
             public boolean onMenuItemClick (MenuItem menuItem){
                 return false;
             }
-
         });
         toolbar.inflateMenu(R.menu.menu);
+
+    }
+
+    private void initNavigationView() {
+        drawerLayout=(DrawerLayout) findViewById(R.id.drawer_Layout);
 
     }
 }
